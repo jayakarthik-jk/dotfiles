2a2b4459fe919a5c22ab3bbe959ca0d09532b92b
Jayakarthik K <iamjayakarthik@gmail.com>
2024-12-28 00:32:19 +0530

    Renamed lsp file

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -25,16 +25,6 @@ return {
 				-- zls = {},		-- brew install zls
 			}
 		},
-		keys = {
-			{ 'K', vim.lsp.buf.hover, mode = 'n', desc = "Hover Text" },
-			{ "gd", vim.lsp.buf.definition, mode = 'n', desc = "Go to definition" },
-			{ "gD", vim.lsp.buf.declaration, mode = 'n', desc = "Go to declaration" },
-			{ "gi", vim.lsp.buf.implementation, mode = 'n', desc = "Go to implementation" },
-			{ "go", vim.lsp.buf.type_definition, mode = 'n', desc = "Go to type definition" },
-			{ "gr", vim.lsp.buf.references, mode = 'n', desc = "References" },
-			{ "gs", vim.lsp.buf.signature_help, mode = 'n', desc = "Signature help" },
-			{ "<space>cr", vim.lsp.buf.rename, mode = 'n', desc = "Rename" },
-		},
 		config = function (_, opts)
 			local lspconfig = require('lspconfig')
 			for server, config in pairs(opts.servers) do
